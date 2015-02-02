d3085180930fa1c445dcb09212004d0f3b564e13
AK <adrianchko@gmail.com>
2015-02-02 21:37:48 +0000

    Create abstract Episode

---
+++ src/Episode.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class Episode {
+
+    protected String url;
+    protected int season;
+    protected int episode;
+    protected List<DownloadOption> options = new LinkedList<>();
+
+    public abstract void parse();
+
+    public List<DownloadOption> getOptions() {
+        return options;
+    }
+
+    public int getSeason() {
+        return season;
+    }
+
+    public void setSeason(int s) {
+        season = s;
+    }
+
+    public int getEpisode() {
+        return episode;
+    }
+
+    public void setEpisode(int e) {
+        episode = e;
+    }
+
+}
