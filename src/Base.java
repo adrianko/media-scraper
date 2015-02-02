993a2b40192e8d0115bd259edd7cbb8c9eb9b4f9
AK <adrianchko@gmail.com>
2015-02-02 20:49:44 +0000

    Move connection to Base

---
+++ src/Base.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Base {
+
+    private static Connection c = null;
+
+    public static Connection get() {
+        if (c == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:db/shows.db");
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return c;
+    }
+
+    public static void close() {
+        try {
+            if (c != null) c.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
