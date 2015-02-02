e9ef5dcddb6e5ea8ddbf91cbe2f77ebb28d44ed3
AK <adrianchko@gmail.com>
2015-02-02 01:23:52 +0000

    Create Show

---
+++ src/Show.java
@@ -0,0 +1,65 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Show {
+
+    private String title;
+    private List<Episode> episodes = new LinkedList<>();
+    private String url;
+
+    public Show(String t, String u) {
+        title = t;
+        url = u;
+        parse();
+    }
+
+    public void parse() {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element main = doc.select(".mainpart").first().select("table").first().select("tr").first();
+            int season = Integer.parseInt(main.select("h3").get(0).text().split(" ")[1].replace(",", ""));
+
+            for (Element e : main.select("div.versionsFolded")) {
+                String stringID = e.select(".infoListCut").first().attr("onclick").replace("showEpisodeInfo(this,", "")
+                    .replace(");", "").replace("'", "").trim();
+                Elements span = e.select(".infoListCut").first().select("span");
+                int epNum = Integer.parseInt(span.get(0).text().split(" ")[1]);
+
+                if (span.size() > 2) {
+                    long timestamp = new SimpleDateFormat("EEEE, MMM dd yyyy").parse(span.get(2).text()).getTime();
+
+                    if (!stringID.equals("") && timestamp < System.currentTimeMillis()) {
+                        Episode ep = new Episode(Scraper.url + stringID);
+
+                        if (ep.getOptions().size() > 0) {
+                            ep.setSeason(season);
+                            ep.setEpisode(epNum);
+                            episodes.add(ep);
+                            break;
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException |ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+}
