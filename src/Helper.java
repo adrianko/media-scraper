94bf6e3e0f4bcafdae4b6a29d46f8297930fa084
AK <adrianchko@gmail.com>
2015-02-04 23:50:20 +0000

    add helper imports

--- src/Helper.java
+++ src/Helper.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Helper {
 
     public static String getCurrentIP() {
