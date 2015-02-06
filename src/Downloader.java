72c9ef89c303e710fc86b3165a9636d60173827d
Adrian Ko <adrianchko@gmail.com>
2015-02-06 15:18:37 +0000

    move into own method

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,2 +1,13 @@
+import java.io.IOException;
+
 public class Downloader {
+    
+    public static void enqueue(String magnet) {
+        try {
+            Runtime.getRuntime().exec("cmd /c start " + magnet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
