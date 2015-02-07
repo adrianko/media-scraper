27f129808f69a209ff8c3bc1a64b2bbff54c7e05
AK <adrianchko@gmail.com>
2015-02-07 17:02:10 +0000

    use helper in kashow

--- src/KAShow.java
+++ src/KAShow.java
@@ -1,9 +1,7 @@
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -16,33 +14,34 @@ public class KAShow extends Show {
 
     public void parse() {
         try {
-            Document doc = Jsoup.connect(url).timeout(Integer.parseInt(Scraper.gSettings.get("timeout"))).get();
-            Element main = doc.select(".mainpart").first().select("table").first().select("tr").first();
-            int season = Integer.parseInt(main.select("h3").get(0).text().split(" ")[1].replace(",", ""));
+            Document doc = Helper.retrievePage(url);
+            if (doc != null) {
+                Element main = doc.select(".mainpart").first().select("table").first().select("tr").first();
+                int season = Integer.parseInt(main.select("h3").get(0).text().split(" ")[1].replace(",", ""));
 
-            for (Element e : main.select("div.versionsFolded")) {
-                String stringID = e.select(".infoListCut").first().attr("onclick").replace("showEpisodeInfo(this,", "")
-                    .replace(");", "").replace("'", "").trim();
-                Elements span = e.select(".infoListCut").first().select("span");
-                int epNum = Integer.parseInt(span.get(0).text().split(" ")[1]);
+                for (Element e : main.select("div.versionsFolded")) {
+                    String stringID = e.select(".infoListCut").first().attr("onclick").replace("showEpisodeInfo(this,", "")
+                            .replace(");", "").replace("'", "").trim();
+                    Elements span = e.select(".infoListCut").first().select("span");
+                    int epNum = Integer.parseInt(span.get(0).text().split(" ")[1]);
 
-                if (span.size() > 2) {
-                    long timestamp = new SimpleDateFormat("EEEE, MMM dd yyyy").parse(span.get(2).text()).getTime();
+                    if (span.size() > 2) {
+                        long timestamp = new SimpleDateFormat("EEEE, MMM dd yyyy").parse(span.get(2).text()).getTime();
 
-                    if (!stringID.equals("") && timestamp < System.currentTimeMillis()) {
-                        KAEpisode ep = new KAEpisode(episodeUrl + stringID);
+                        if (!stringID.equals("") && timestamp < System.currentTimeMillis()) {
+                            KAEpisode ep = new KAEpisode(episodeUrl + stringID);
 
-                        if (ep.getOptions().size() > 0) {
-                            ep.setSeason(season);
-                            ep.setEpisode(epNum);
-                            episodes.add(ep);
-                            break;
+                            if (ep.getOptions().size() > 0) {
+                                ep.setSeason(season);
+                                ep.setEpisode(epNum);
+                                episodes.add(ep);
+                                break;
+                            }
                         }
                     }
                 }
             }
-
-        } catch (IOException | ParseException e) {
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
