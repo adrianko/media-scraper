4a730bf4fd63ab23ed17367d76be98f488e3dcb4
AK <adrianchko@gmail.com>
2015-02-07 17:13:30 +0000

    remove continuation

--- src/KAEpisode.java
+++ src/KAEpisode.java
@@ -13,10 +13,8 @@ public class KAEpisode extends Episode {
 
         if (doc != null) {
             for (Element e : doc.select("tr.odd, tr.even")) {
-                String name = e.select("td").first().select(".torrentname").first().select("a.cellMainLink")
-                    .first().text();
-                String magnet = e.select("td").first().select(".iaconbox").first().select("a.imagnet").first()
-                    .attr("href").split("&")[0];
+                String name = e.select("td").first().select(".torrentname").first().select("a.cellMainLink").first().text();
+                String magnet = e.select("td").first().select(".iaconbox").first().select("a.imagnet").first().attr("href").split("&")[0];
                 String size = e.select("td.nobr").first().text().replaceAll("\\s+", "");
 
                 options.add(new DownloadOption(name, magnet, size));
