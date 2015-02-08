63f4fae25647a95de4d3c1370d5f829c6aff9802
AK <adrianchko@gmail.com>
2015-02-08 19:03:12 +0000

    Add getAddURL method

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class Downloader {
     
@@ -8,6 +10,18 @@ public class Downloader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getAddURL(String magnet) {
+        try {
+            return "http://" + Scraper.settings.get("dl_user") + ":" + Scraper.settings.get("dl_pass") + "@" +
+                Scraper.settings.get("dl_host") + ":" + Scraper.settings.get("dl_port") + "/gui/?action=add-url&s=" +
+                URLEncoder.encode(magnet, "US-ASCII") + "&t=" + System.currentTimeMillis();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
     
 }
