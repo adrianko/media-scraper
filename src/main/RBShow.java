46d58d12cacb31648c34be1c00d69792197ebf19
AK <adrianchko@gmail.com>
2015-02-16 21:45:16 +0000

    Extend Show, super and override

--- src/main/RBShow.java
+++ src/main/RBShow.java
@@ -1,4 +1,14 @@
 package main;
 
-public class RBShow {
+public class RBShow extends Show {
+
+    public RBShow(String t, String u, String eu, int s, int e, int h, int r) {
+        super(t, u, eu, s, e, h,r);
+    }
+
+    @Override
+    public void parse() {
+
+    }
+
 }
