6d205b1ba052cd287392b8c9bc634dad5e9e7e6a
AK <adrianchko@gmail.com>
2015-02-16 19:53:03 +0000

    use Scraper stash

--- src/DB.java
+++ src/DB.java
@@ -27,18 +27,10 @@ public class DB {
     
     public static void bump(String title) {
         try {
-            PreparedStatement retrieve = get().prepareStatement("SELECT episode FROM shows WHERE title = ?");
-            retrieve.setString(1, title);
-            ResultSet rs = retrieve.executeQuery();
-            rs.first();
-            
             PreparedStatement update = get().prepareStatement("UPDATE shows SET episode = ? WHERE title = ?");
-            update.setInt(1, (rs.getInt("episode") + 1));
+            update.setInt(1, Scraper.shows.get(title).getEpisode() + 1);
             update.setString(2, title);
             update.executeUpdate();
-            
-            rs.close();
-            retrieve.close();
             update.close();
         } catch (SQLException e) {
             e.printStackTrace();
