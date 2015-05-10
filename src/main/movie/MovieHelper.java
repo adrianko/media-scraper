4a9ac3f7642d485345fc29e0f849fbfd6f38491f
AK <adrianchko@gmail.com>
2015-05-10 17:57:16 +0100

    Load MovieSettings

--- src/main/movie/MovieHelper.java
+++ src/main/movie/MovieHelper.java
@@ -1,11 +1,26 @@
 package main.movie;
 
 import main.Helper;
+import main.Database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MovieHelper extends Helper {
 
     public void loadSettings() {
+        try (Statement s = Database.get("movies").createStatement()) {
+            ResultSet dbSettings = s.executeQuery("SELECT * FROM settings");
 
+            while (dbSettings.next()) {
+                settings.put(dbSettings.getString("property"), dbSettings.getString("value"));
+            }
+
+            dbSettings.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
