73a017f9a09336a306f023c58a2723e017084571
AK <adrianchko@gmail.com>
2015-05-11 17:32:02 +0100

    addCacheItem to db

--- src/main/movie/MovieDatabase.java
+++ src/main/movie/MovieDatabase.java
@@ -2,6 +2,7 @@ package main.movie;
 
 import main.Database;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
@@ -23,6 +24,42 @@ public class MovieDatabase extends Database {
         }
 
         return movies;
+    }
+
+    public static Set<CacheItem> getCache() {
+        Set<CacheItem> cache = new HashSet<>();
+
+        try {
+            ResultSet rs = get("movies").createStatement().executeQuery("SELECT * FROM cache");
+
+            while (rs.next()) {
+                cache.add(new CacheItem(rs.getInt("id"), rs.getString("title"), rs.getString("magnet")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return cache;
+    }
+
+    public boolean addCacheItem(CacheItem ci) {
+        try {
+            PreparedStatement check = get("movies").prepareStatement("SELECT * FROM cache WHERE id=?");
+            check.setInt(1, ci.getID());
+            ResultSet checkRS = check.executeQuery();
+            int rowCount = checkRS.last() ? checkRS.getRow() : 0;
+
+            if (rowCount == 0) {
+                PreparedStatement add = get("movies").prepareStatement("INSERT INTO cache (id, title, magnet) VALUES " +
+                        "(?, ?, ?)");
+                add.setInt(1, ci.getID());
+                add.setString(2, ci.getTitle());
+                add.setString(3, ci.getMagnet());
+                add.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
