9559828f2c5f6e903f88b7fd3d7325ed7d48d3f7
Adrian Ko <adrianchko@gmail.com>
2015-05-19 07:47:20 +0100

    Add basic server

--- src/main/ui/HTTPServer.java
+++ src/main/ui/HTTPServer.java
@@ -1,9 +1,19 @@
 package main.ui;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 public class HTTPServer {
     
     public static void main(String[] args) {
-               
+        try {
+            HttpServer.create(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), 9898), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
