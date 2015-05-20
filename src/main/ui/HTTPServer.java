499e511e2b4acc007c44587c77aff9550dd19d2c
Adrian Ko <adrianchko@gmail.com>
2015-05-20 07:46:56 +0100

    Add main invocation from web ui

--- src/main/ui/HTTPServer.java
+++ src/main/ui/HTTPServer.java
@@ -3,6 +3,7 @@ package main.ui;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import main.scrapers.tv.Scraper;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -16,6 +17,7 @@ public class HTTPServer {
         try {
             server = HttpServer.create(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), 9898), 0);
             server.createContext("/", new Home());
+            server.createContext("/scrape", new Scrape());
             server.setExecutor(null);
             server.start();
         } catch (IOException e) {
@@ -38,6 +40,25 @@ public class HTTPServer {
             String response = "Hello";
             
             try {
+                t.getResponseHeaders().add("Content-Type", "text/html");
+                t.sendResponseHeaders(200, response.length());
+                t.getResponseBody().write(response.getBytes());
+                t.getResponseBody().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+    
+    class Scrape implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            Scraper.main(new String[]{});
+
+            try {
+                String response = "1";
                 t.getResponseHeaders().add("Content-Type", "text/html");
                 t.sendResponseHeaders(200, response.length());
                 t.getResponseBody().write(response.getBytes());
