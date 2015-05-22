bd042e0ff332401b75ae94a095562ea9a0ff3b3a
Adrian Ko <adrianchko@gmail.com>
2015-05-22 08:12:46 +0100

    Add string combo tests

--- src/test/ui/core/components/HelperTest.java
+++ src/test/ui/core/components/HelperTest.java
@@ -15,7 +15,7 @@ public class HelperTest {
     }
 
     @Test
-    public void shaNumeric() {
+    public void sha1Numeric() {
         String exp = "b835c672565363230d65528838a0206f53bef988";
         String act = Helper.sha1("859023789474");
 
@@ -23,10 +23,42 @@ public class HelperTest {
     }
     
     @Test
-    public void sha1Alphanumeric() {
+    public void sha1Symbol() {
+        String exp = "1931f83e8760fa9c4f7ee74156b90828cb19e570";
+        String act = Helper.sha1("&(*@£&($*&(*£");
+
+        Assert.assertEquals(exp, act);
+    }
+    
+    @Test
+    public void sha1AlphaNumeric() {
         String exp = "d33b97f878d22fbf0b978b26ea04a8d2f544e7f9";
         String act = Helper.sha1("S9H3b3H0Lk");
         
+        Assert.assertEquals(exp, act);
+    }
+    
+    @Test 
+    public void sha1AlphaSymbol() {
+        String exp = "23ad839d22975a0ad75bae82f7bde9b93b9113eb";
+        String act = Helper.sha1("!PY£h$ioK)OR@nR");
+
+        Assert.assertEquals(exp, act);
+    }
+    
+    @Test
+    public void sha1NumericSymbol() {
+        String exp = "b85c6dfe2ab47452916e2d1d4c3a9c4bdbb32459";
+        String act = Helper.sha1("798^%)(&%8^06)^&7(");
+
+        Assert.assertEquals(exp, act);
+    }
+    
+    @Test
+    public void sha1AlphaNumericSymbol() {
+        String exp = "1e65a9370e914afc9042ddfcaa3dc1819cdf5034";
+        String act = Helper.sha1("5E=9CKkc*{tPj74R");
+
         Assert.assertEquals(exp, act);
     }
     
