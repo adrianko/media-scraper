7925fe2a7c82fe5ee49c5b3f8a070715e8bd692b
Adrian Ko <adrianchko@gmail.com>
2015-05-22 07:59:36 +0100

    Add MessageDigest method

--- src/main/ui/core/components/Helper.java
+++ src/main/ui/core/components/Helper.java
@@ -1,4 +1,26 @@
 package main.ui.core.components;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Helper {
+    
+    public static String sha1(String message) {
+        StringBuilder stringBuffer = new StringBuilder();
+        
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.reset();
+            md.update(message.getBytes());
+
+            for (byte aB : md.digest()) {
+                stringBuffer.append(Integer.toString((aB & 0xff) + 0x100, 16).substring(1));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return stringBuffer.toString();
+    }
+    
 }
