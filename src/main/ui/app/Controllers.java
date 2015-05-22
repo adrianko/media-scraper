26f20cae305704877f652b19fae658e98a215b20
AK <adrianchko@gmail.com>
2015-05-22 18:03:03 +0100

    Add json response to /api context

--- src/main/ui/app/Controllers.java
+++ src/main/ui/app/Controllers.java
@@ -4,6 +4,10 @@ import com.sun.net.httpserver.HttpExchange;
 import main.scrapers.tv.TVScraper;
 import main.ui.core.components.Controller;
 import main.ui.core.components.Response;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Controllers {
 
@@ -21,7 +25,19 @@ public class Controllers {
         @Override
         public void handle(HttpExchange t) {
             TVScraper.main(new String[]{});
-            Response.send(t, "{success: 1}", "application/json");
+            Response.send(t, "{\"success\": 1}", "application/json");
+        }
+
+    }
+
+    public static class API extends Controller {
+
+        @Override
+        public void handle(HttpExchange t) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", "1");
+            response.put("request", t.getRequestURI().toString());
+            Response.send(t, new JSONObject(response).toString(), "application/json");
         }
 
     }
