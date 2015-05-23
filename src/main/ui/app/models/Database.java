e9eaf12bda228bc7110aecfe6ed7aeb59f2e7fad
Adrian Ko <adrianchko@gmail.com>
2015-05-23 21:23:16 +0100

    Add getSettings for specific db

--- src/main/ui/app/models/Database.java
+++ src/main/ui/app/models/Database.java
@@ -1,7 +1,14 @@
 package main.ui.app.models;
 
 import main.scrapers.tv.shows.Show;
+import main.ui.Base;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,8 +24,23 @@ public class Database {
         return new LinkedList<>();
     }
     
-    public Map<String, String> getSettings(String category) {
-        return new HashMap<>();
+    public Map<String, String> getSettings(String db) {
+        Map<String, String> settings = new HashMap<>();
+        
+        if (Files.exists(Paths.get(Base.path + db + ".db"))) {
+            try {
+                Connection c = DriverManager.getConnection("jdbc:sqlite:" + Base.path + "/db/" + db + ".db");
+                ResultSet rs = c.createStatement().executeQuery("SELECT * FROM settings");
+                
+                while (rs.next()) {
+                    settings.put(rs.getString("property"), rs.getString("value"));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return settings;
     }
     
     public Map<String, String> getGlobalSettings() {
