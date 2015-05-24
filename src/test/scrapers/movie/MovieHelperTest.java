e6cf2830b9d7c222c935c5ef4bdb94d95ecb963f
AK <adrianchko@gmail.com>
2015-05-24 20:38:50 +0100

    More sanitizeString tests

--- src/test/scrapers/movie/MovieHelperTest.java
+++ src/test/scrapers/movie/MovieHelperTest.java
@@ -23,7 +23,7 @@ public class MovieHelperTest {
     }
 
     @Test
-    public void sanitizeStringDashPrefix() {
+    public void sanitizeStringSpacePrefix() {
         String exp = "aaaa";
         String act = MovieHelper.sanitizeString("  aaaa");
 
@@ -31,9 +31,73 @@ public class MovieHelperTest {
     }
 
     @Test
-    public void sanitizeStringDashSuffix() {
+    public void sanitizeStringSpaceSuffix() {
         String exp = "aaaa";
         String act = MovieHelper.sanitizeString("aaaa    ");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringSpacePrefixSuffix() {
+        String exp = "aaaa";
+        String act = MovieHelper.sanitizeString("    aaaa    ");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringSymbolBetween() {
+        String exp = "aa-aa";
+        String act = MovieHelper.sanitizeString("aa:aa");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringDoubleSymbolBetween() {
+        String exp = "aa-aa";
+        String act = MovieHelper.sanitizeString("aa:;aa");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringSymbolBetweenPrefix() {
+        String exp = "aa-aa";
+        String act = MovieHelper.sanitizeString("(aa:;aa");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringSymbolBetweenSuffix() {
+        String exp = "aa-aa";
+        String act = MovieHelper.sanitizeString("aa:;aa)");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringSymbolBetweenPrefixSuffix() {
+        String exp = "aa-aa";
+        String act = MovieHelper.sanitizeString("(aa:;aa)");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringTitleNoSymbols() {
+        String exp = "some-title-2009";
+        String act = MovieHelper.sanitizeString("some title (2009)");
+
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void sanitizeStringTitleSymbols() {
+        String exp = "some-title-2-another-title-2010";
+        String act = MovieHelper.sanitizeString("Some Title 2: Another Title (2010)");
 
         Assert.assertEquals(exp, act);
     }
