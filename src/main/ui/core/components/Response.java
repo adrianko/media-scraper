35b9c79d8aa78f5fac953b3dc2faddcfee474080
Adrian Ko <adrianchko@gmail.com>
2015-05-26 07:52:07 +0100

    Create sendFile method

--- src/main/ui/core/components/Response.java
+++ src/main/ui/core/components/Response.java
@@ -4,19 +4,26 @@ import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class Response {
-
-    public static void send(HttpExchange t, String response, String contentType) {
+    
+    public static void setHeaders(HttpExchange t, byte[] response, String contentType) {
         try {
             t.getResponseHeaders().add("Content-Type", contentType);
-            t.sendResponseHeaders(200, response.length());
-            t.getResponseBody().write(response.getBytes());
-            t.getResponseBody().close();
+            t.sendResponseHeaders(200, response.length);
+            t.getResponseBody().write(response);
+            t.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void send(HttpExchange t, String response, String contentType) {
+        setHeaders(t, response.getBytes(), contentType);
     }
 
     public static void sendJSON(HttpExchange t, Map<String, Object> json) {
@@ -25,6 +32,16 @@ public class Response {
 
     public static void sendHTML(HttpExchange t, String html) {
         Response.send(t, html, "text/html");
+    }
+
+    public static void sendFile(HttpExchange t, String filePath) {
+        try {
+            Path path = Paths.get(filePath);
+            byte[] file = Files.readAllBytes(path);
+            setHeaders(t, file, Files.probeContentType(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
