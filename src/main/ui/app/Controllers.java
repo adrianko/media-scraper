3f8a649f2498a1bd5c4e759e4f60b0c6035aa948
AK <adrianchko@gmail.com>
2015-05-27 21:03:33 +0100

    Remove unused imports

--- src/main/ui/app/Controllers.java
+++ src/main/ui/app/Controllers.java
@@ -1,18 +1,11 @@
 package main.ui.app;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
 import com.sun.net.httpserver.HttpExchange;
 import main.scrapers.tv.TVScraper;
-import main.ui.Base;
 import main.ui.core.components.Controller;
 import main.ui.core.components.Helper;
 import main.ui.core.components.Response;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
