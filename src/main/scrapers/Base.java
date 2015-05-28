8393d9cb44fd2549bf73cde4d2f3f16bdac6ec4f
Adrian Ko <adrianchko@gmail.com>
2015-05-28 07:42:20 +0100

    Match Base in ui

--- src/main/scrapers/Base.java
+++ src/main/scrapers/Base.java
@@ -1,7 +1,11 @@
 package main.scrapers;
 
 public class Base {
-    
-    public static String path = Base.class.getResource(".").getPath() + "../../../../../";
+
+    public static String os = System.getProperty("os.name");
+
+    public static String path = os.contains("Windows") && Base.class.getResource(".").getPath().startsWith("/") ?
+            (Base.class.getResource(".").getPath() + "../../../../../").substring(1) :
+            Base.class.getResource(".").getPath() + "../../../../../";
     
 }
