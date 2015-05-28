022100bc8ae5f3b293ab6c673ce23546568d3d6c
AK <adrianchko@gmail.com>
2015-05-28 19:17:49 +0100

    Fix path substring

--- src/main/scrapers/Base.java
+++ src/main/scrapers/Base.java
@@ -4,8 +4,8 @@ public class Base {
 
     public static String os = System.getProperty("os.name");
 
-    public static String path = os.contains("Windows") && Base.class.getResource(".").getPath().startsWith("/") ?
-            (Base.class.getResource(".").getPath() + "../../../../../").substring(1) :
-            Base.class.getResource(".").getPath() + "../../../../../";
+    public static String path = (os.contains("Windows") && Base.class.getResource(".").getPath().startsWith("/") ?
+            Base.class.getResource(".").getPath().substring(1) : Base.class.getResource(".").getPath())
+            + "../../../../../";
     
 }
